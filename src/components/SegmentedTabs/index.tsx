cbc0829aff270f534bca344dd6749e94bad3110f
Dinkar <aakashdinkarh@gmail.com>
2023-12-18 10:18:17 +0530

    minor destructuring changes

--- src/components/SegmentedTabs/index.tsx
+++ src/components/SegmentedTabs/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import styles from './styles.module.css';
 
-const { isValidElement, Children } = React;
+const { isValidElement, Children, useState, useRef, useEffect } = React;
 
 interface SegmentedTabsProps {
 	defaultActiveTab: string;
@@ -30,11 +30,11 @@ function SegmentedTabs ({
 	const tabs: Array<{ name: string, title?: string }> = validChildren
 		.map((child: any) => ({ name: child.props.name ?? '', title: child.props.title ?? '' }));
 
-	const [activeTab, setActiveTab] = React.useState<string>(defaultActiveTab ?? tabs[0].name);
+	const [activeTab, setActiveTab] = useState<string>(defaultActiveTab ?? tabs[0].name);
 
-	const tabsRef: Record<string, any> = React.useRef({});
+	const tabsRef: Record<string, any> = useRef({});
 
-	React.useEffect(() => {
+	useEffect(() => {
 		const currentButton = tabsRef.current?.[activeTab];
 
 		if (currentButton != null) {
